8d63f326774c668ed5ddf9fbaedcf192ab494a6c
pyu666 <32445749+pyu666@users.noreply.github.com>
2020-08-23 00:00:01 +0900

    目的地候補追加、選択可能に

--- saikoro/js/main.js
+++ saikoro/js/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
-const airportList = ["新千歳", "利尻", "稚内", "紋別", "女満別", "旭川", "中標津", "釧路", "帯広", "函館", "奥尻", "青森", "大館能代", "三沢", "秋田", "花巻", "仙台", "山形", "庄内", "福島", "羽田", "成田", "八丈島", "新潟", "松本", "小松", "静岡", "名古屋", "伊丹", "大阪", "神戸", "南紀白浜", "但馬", "隠岐", "岡山", "出雲", "広島", "山口宇部", "徳島", "高松", "高知", "松山", "福岡", "北九州", "大分", "長崎", "熊本", "天草", "宮崎", "鹿児島", "種子島", "屋久島", "喜界島", "奄美大島", "徳之島", "沖永良部", "与論", "那覇", "北大東", "南大東", "久米島", "宮古", "多良間", "石垣", "与那国"];
-
+const syuyochiList =["東京/羽田","大阪/伊丹","福岡/博多","仙台","札幌/新千歳","名古屋","那覇"];
+const airportList = ["利尻", "稚内", "紋別", "女満別", "旭川", "中標津", "釧路", "帯広", "函館", "奥尻", "青森", "大館能代", "三沢", "秋田", "花巻", "仙台", "山形", "庄内", "福島", "成田", "八丈島", "新潟", "松本", "小松", "静岡", "南紀白浜", "但馬", "隠岐", "岡山", "出雲", "広島", "山口宇部", "徳島", "高松", "高知", "松山", "北九州", "大分", "長崎", "熊本", "天草", "宮崎", "鹿児島", "種子島", "屋久島", "喜界島", "奄美大島", "徳之島", "沖永良部", "与論", "那覇", "北大東", "南大東", "久米島", "宮古", "多良間", "石垣", "与那国"];
+const shinkansenStationList = ["新函館北斗","八戸","新青森","盛岡","大宮","岡山","広島","小倉","秋田","新庄","宇都宮","高崎","軽井沢","長野","富山","金沢","新潟","越後湯沢"];
+const destinationList = syuyochiList.concat(airportList.concat(shinkansenStationList));
 function changetxt(id, val) {
     document.getElementById(val).innerHTML = id.value;
     makeTweetText();
@@ -9,32 +11,49 @@ function changetxt(id, val) {
 
 function detectKouho() {
     //現在地及び目的地の取得し、候補リストを作成
-    const here2 = document.getElementById("here");
-    const hereValue = here2.options[here2.selectedIndex].value;
+    const here = document.getElementById("here");
+    const hereValue = here.options[here.selectedIndex].value;
     const destination = document.getElementById("destination");
     const destinationValue = destination.options[destination.selectedIndex].value;
     const chance = document.getElementById("chance");
     const chanceValue = chance.options[chance.selectedIndex].value;
     console.log('現在地 = ' + hereValue + ', ' + '目的地 = ' + destinationValue);
     //目的地の候補リスト作成
-    const hereindex = airportList.indexOf(hereValue);
-    const kouhochi = airportList.filter(n => n !== airportList[hereindex]);
-    const destinationindex = kouhochi.indexOf(destinationValue);
-    makeLandomList(6, kouhochi, destinationindex, chanceValue);
+    let checkeddestinationList = [];
+    let isChecked = false;
+    if(document.getElementById("isShuyouchi").checked){
+        isChecked = true;
+        checkeddestinationList = checkeddestinationList.concat(syuyochiList);
+    }if (document.getElementById("isAirport").checked){
+        isChecked = true;
+        checkeddestinationList = checkeddestinationList.concat(airportList);
+    }if (document.getElementById("isShinkansen").checked){
+        isChecked = true;
+        checkeddestinationList = checkeddestinationList.concat(airportList);
+    }if (isChecked === false){
+        checkeddestinationList = checkeddestinationList.concat(destinationList);
+    }
+    console.log(checkeddestinationList);
+    const hereindex = destinationList.indexOf(hereValue);
+    const kouhochi = checkeddestinationList.filter(n => n !== destinationList[hereindex]);
+    //const destinationValue = kouhochi.indexOf(destinationValue);
+    console.log(kouhochi,isChecked);
+    makeRandomList(6, kouhochi, destinationValue, chanceValue);
 }
 
 function makepulldownList(name) {
-    const airport = document.getElementById(name);
+    const destination = document.getElementById(name);
     document.createElement('option');
-    for (var i = 0; i < airportList.length; i++) {
+    for (var i = 0; i < destinationList.length; i++) {
         const option = document.createElement('option');
-        option.setAttribute('value', airportList[i]);
-        option.innerHTML = airportList[i];
-        airport.appendChild(option);
+        option.setAttribute('value', destinationList[i]);
+        option.innerHTML = destinationList[i];
+        destination.appendChild(option);
     }
+
 }
 
-function makeLandomList(number, array, destinationIndex, chanceNumber) {
+function makeRandomList(number, array, destination, chanceNumber) {
     var randomArray = array.slice();
     for (var i = randomArray.length - 1; i > 0; i--) {
         var r = Math.floor(Math.random() * (i + 1));
@@ -47,10 +66,10 @@ function makeLandomList(number, array, destinationIndex, chanceNumber) {
         document.getElementById('deme' + (i + 1)+'sub').innerHTML =  i + 1 +'の目が出たら？';
     }
     //チャンスタイム処理
-    if (destinationIndex != -1) {
+    if (destination != "None") {
         for (var i = 0; i < chanceNumber; i++) {
             const detecedNumber = Math.floor(Math.random() * number + 1);
-            document.getElementById('deme' + (detecedNumber)).innerHTML = array[destinationIndex];
+            document.getElementById('deme' + (detecedNumber)).innerHTML = destination;
         }
     }
 }
